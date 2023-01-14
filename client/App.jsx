67e17a6bd964abe2fdd255007115ba75f1f07275
Hannah Spencer <hannah.spencer2000@gmail.com>
2023-01-14 10:43:00 -0800

    Frontend notes

--- client/App.jsx
+++ client/App.jsx
@@ -4,5 +4,23 @@ function App() {
     
 }
 
+ 
+// sign in page
+ //username
+ //password
+ //submit button
+// sign up page
+ //username
+ //password
+ //submit button
+// homepage
+// default state to 0 for every category
+ //num for food, drinks, space, sound, outlets, parking, wifi
+   //onChange(handleChange) for each category handleChange updates state
+ //submit sends get request referencing state to backend
+   //onSubmit (event)
+ //
+ 
+
 
 export default App;
