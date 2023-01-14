a31dec2602768861a1645eb6e62fc76a29363e1d
Jaden Nguyen <jadenthai206@gmail.com>
2023-01-13 20:46:37 -0800

    authController handles user authentication

---
+++ server/controllers/authController.js
@@ -0,0 +1,13 @@
+const db = require('./sqlconnection.js')
+
+authController = {};
+
+authController.createUser = (req, res, next) => {
+  
+}
+
+authController.verifyUser = (req, res, next) => {
+  
+}
+
+module.exports = authController;
