c270f7a3f8459efb70124081b267bd1cd2135d36
Jaden Nguyen <jadenthai206@gmail.com>
2023-01-13 18:48:45 -0800

    Create index.js

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter as Route } from "react-router-dom";
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)
