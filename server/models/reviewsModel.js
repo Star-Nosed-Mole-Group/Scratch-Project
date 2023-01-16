cb7cb614e0a7bcd70f395de7d6e92af4abee7741
Jaden Nguyen <jadenthai206@gmail.com>
2023-01-16 03:52:55 -0800

    added comments to visualize the models
    Co-Authored-By: cynnsarah <115136514+cynnsarah@users.noreply.github.com>
    Co-Authored-By: Mark Bryan <102488255+mbryan13@users.noreply.github.com>
    Co-Authored-By: mhenely <109127719+mhenely@users.noreply.github.com>
    Co-Authored-By: Hannahspen <114366921+Hannahspen@users.noreply.github.com>

--- server/models/reviewsModel.js
+++ server/models/reviewsModel.js
@@ -9,8 +9,33 @@ const reviewsSchema = new Schema({
     sound: {type: Number, default: 0},
     outlets: {type: Number, default: 0},
     parking: {type: Number, default: 0},
-    wifi: {type: Number, default: 0}
+    wifi: {type: Number, default: 0},
+    username: {type: String, required: true}
 })
 
 const Reviews = mongoose.model('Reviews', reviewsSchema);
 module.exports = Reviews;
+
+
+/**
+ *    table spots current data in psql
+  _id | food | drinks | space | sound | outlets | parking | wifi |     name      
+-----+------+--------+-------+-------+---------+---------+------+---------------
+   1 |    3 |      3 |     3 |     4 |       4 |       2 |    3 | Starbucks
+   2 |    2 |      3 |     4 |     3 |       4 |       1 |    2 | Peet"s Coffee 
+ * 
+      reviewsModel -> reviews DB current data
+  { [
+    shopId: 1,
+    food: 1,
+    drinks: 2,
+    space: 3,
+    sound: 2,
+    outlets: 1,
+    parking: 1
+    wifi: 1,
+    username: 'harsh_critic123'
+  ]
+}
+
+ */
