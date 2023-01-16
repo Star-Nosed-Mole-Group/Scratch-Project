d16851792c7c5329486629129869856579dda03f
Jaden Nguyen <jadenthai206@gmail.com>
2023-01-16 03:49:54 -0800

    added a shoplist component to render all requested shops

---
+++ client/components/ShopList.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import '../stylesheets/shoplist.css';
+
+const ShopList = props => {
+
+//   const [ quality_meals, quality_drinks, space, sound, outlets, parking, wifi, shopname ] = props;  
+  console.log(props);
+  return (
+    <h1>List of all shops</h1>
+  )
+}
+
+export default ShopList;
