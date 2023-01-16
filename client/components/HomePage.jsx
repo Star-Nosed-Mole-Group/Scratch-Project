0da7a8bc4d79d20bd49e7bf11b0e41e61be748bd
Jaden Nguyen <jadenthai206@gmail.com>
2023-01-16 03:49:23 -0800

    added the search for name element

--- client/components/HomePage.jsx
+++ client/components/HomePage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
+import '../stylesheets/homepage.css'
 
 class searchForm extends React.Component {
     
@@ -11,12 +13,14 @@ class searchForm extends React.Component {
             sound: 1,
             outlets: 1,
             parking: 1,
-            wifi: 1
+            wifi: 1,
+            shopname: ''
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeSearch = this.handleChangeSearch.bind(this);
     }
-    
+  
+
     handleChange(event) {
         const target = event.target;
         const value = target.value;
@@ -24,18 +28,26 @@ class searchForm extends React.Component {
         this.setState({
             [name]: value
         });
+        console.log(JSON.stringify(this.state));
     }
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.quality_meals);
+    handleChangeSearch(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+        console.log(JSON.stringify(this.state));
     }
 
     render () {
 
         return (
-        <div className="homePageForm">
-            <form onSubmit={this.handleSubmit}>
-                <label>Minimun rating for: </label>
+        <div className="homePageForms">
+          <div className="searchByCriteriaForms">
+            <form>
+                <label>Minimum rating for: </label>
                 <br/>
               <label>
                 Meal quality: 
@@ -72,8 +84,21 @@ class searchForm extends React.Component {
                 <input type='number' name='wifi' value={this.state.wifi} onChange={this.handleChange} />
               </label>
               <br />
-              <input type='submit' value='Search'/>
+                <div className="searchCriteria-btn">
+                  <Link to={'/shoplist'}  query={ { state: this.state }}><button>Search</button></Link>
+                </div>
             </form>
+          </div>
+          <div className="homepageSearchForm">
+            <input type='text' className="search-bar" value={this.state.searchName} onChange={this.handleChangeSearch} placeholder="Search coffee shop name..." />
+              <Link to={'/shoplist'} params={ { state: this.state }} >
+                <button className="search-bar-button">
+                  <svg stroke="currentColor" fill="currentColor" viewBox="0 0 1024 1024" height="1.0em" width="1.0em" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
+                  </svg>
+                </button>
+              </Link>
+          </div>
         </div>
         )
     }
