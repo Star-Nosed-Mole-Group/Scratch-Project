b533f63bbd98e7e95ec46bd9051c4c7f7d2c931b
Sarah Cynn <sarah@Sarahs-MacBook-Pro.local>
2023-01-15 22:48:26 -0800

    added coffee controllers and server route handlers

--- server/routes/coffee.js
+++ server/routes/coffee.js
@@ -3,11 +3,18 @@ const express = require('express');
 const app = express.Router();
 const coffeeController = require('../controllers/coffeeController');
 
+//i need help on figuring out the routes and what controllers should flow together
+
 //user sets criteria and sends get request to see filtered coffeeshops
-app.get('/', coffeeController.readCoffeeShops, (req, res) => { 
+app.get('/', coffeeController.searchShopsByCriteria, (req, res) => { 
+    res.status(200).json(res.locals.readShops) //depending on the middleware, is this the correct corresponding response?
+})
+
+app.get('/', coffeeController.searchShopsByName, (req, res) => {
     res.status(200).json(res.locals.readShops)
 })
 
+//intuitively should this endpoint be something like /shops?
 app.get('/reviews', coffeeController.readReviews, (req, res) => {
     res.status(200).json(res.locals.reviews)
 })
