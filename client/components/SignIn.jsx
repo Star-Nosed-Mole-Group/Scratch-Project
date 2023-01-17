55c1c0c7e6697df83b1117bf657ac108501f82f2
Hannah Spencer <hannah.spencer2000@gmail.com>
2023-01-16 19:26:39 -0800

    Signin functionality
    Co-authored-by: mhenely <mhenely@users.noreply.github.com>

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -1,70 +1,143 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 import '../stylesheets/signin.css'
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 
 
-class SignIn extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            username: "",
-            password: ""
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+
+const SignIn = (props) => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const navigate = useNavigate();
+  
+  const handleChange = (event) => {
+     if (event.target.name == 'username') {
+        setUsername(event.target.value);
+     } else {
+        setPassword(event.target.value)
+     }
     }
-
-    handleChange(event) {
-        const target = event.target;
-        // alert(' Event is ' + event.target.)
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-            [name]: value
+   // const navigate = useNavigate();
+    
+    const handleSubmit = (event) => {
+        //alert('Username: ' + username + ' Password: ' + password);
+        const requestOptions = {
+            // mode: 'no-cors',
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({username, password})
+            };
+        fetch('http://localhost:3000/api/user/login', requestOptions)
+          .then(data => {
+            if (!data.ok) {
+                alert('Your username or password is incorrect');
+                //throw Error('Your username or password is incorrect')
+                navigate('/signin')
+            } else {
+               navigate('/home')
+            }
         })
-    }
-
-    // use this to log in -> username: hello  / password: beans
-    handleSubmit() {
-      const requestOptions = {
-        // mode: 'no-cors',
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(this.state)
-      };
-      fetch('http://localhost:3000/api/user/login', requestOptions)
         .catch(err => console.log(err));
         // alert('Make get request');
-    }
+    } 
 
-    render () {
-
-        return (
-            <div className="login">
-                <h1>Welcome to Coffee Shop</h1>
+    return (
+        <div className="login">
+            <h1>Welcome to Coffee Shop</h1>
                 <div className="username">
-                <label htmlFor="username">Username</label><br></br>
-                <input placeholder='Enter your username' type="text" id="username" name="username" onChange={(e) => this.handleChange(e)}></input>
+                    <label htmlFor="username">Username</label><br></br>
+                    <input placeholder='Enter your username' type="text" id="username" name="username" onChange={(e) => handleChange(e)}></input>
                 </div>
-            <div className="password">
-                <label htmlFor="password">Password</label><br></br>
-                <input placeholder='Enter your password' type="password" id="password" name="password" onChange={(e) => this.handleChange(e)}></input>
-            </div><br></br>
-            <div className="signin-btn">
-              <Link to={'/home'}><button onClick={this.handleSubmit}>Sign in</button></Link>
+                <div className="password">
+                    <label htmlFor="password">Password</label><br></br>
+                    <input placeholder='Enter your password' type="password" id="password" name="password" onChange={(e) => handleChange(e)}></input>
+                </div><br></br>
+                <div className="signin-btn">
+              <button onClick={handleSubmit}>Sign in</button>
             </div>
-                <div>
-                 <Link to="/signup">
-                    <button>Don't have an account?</button>
-                 </Link>
-                 <Link to="/home">
-                    <button>So we can still access home</button>
-                 </Link>
-                </div>
+            <div>
+             <Link to="/signup">
+                <button>Don't have an account?</button>
+             </Link>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+
+// class SignIn extends React.Component {
+//     constructor (props) {
+//         super(props)
+//         this.state = {
+//             username: "",
+//             password: ""
+//         }
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleChange(event) {
+//         const target = event.target;
+//         // alert(' Event is ' + event.target.)
+//         const value = target.value;
+//         const name = target.name;
+//         this.setState({
+//             [name]: value
+//         })
+//     }
+
+//     // use this to log in -> username: hello  / password: beans
+//     handleSubmit() {
+//       const requestOptions = {
+//         // mode: 'no-cors',
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(this.state)
+//       };
+//       const navigate = useNavigate();
+//       fetch('http://localhost:3000/api/user/login', requestOptions)
+//         .then(data => {
+//             if (!data.ok) {
+//                 alert('Your username or password is incorrect');
+//                 navigate('/signin')
+//             } else {
+//                 navigate('/home')
+//             }
+//         })
+//         .catch(err => console.log(err));
+//         // alert('Make get request');
+//     }
+
+//     render () {
+
+//         return (
+//             <div className="login">
+//                 <h1>Welcome to Coffee Shop</h1>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <div className="username">
+//                         <label htmlFor="username">Username</label><br></br>
+//                         <input placeholder='Enter your username' type="text" id="username" name="username" onChange={(e) => this.handleChange(e)}></input>
+//                     </div>
+//                     <div className="password">
+//                         <label htmlFor="password">Password</label><br></br>
+//                         <input placeholder='Enter your password' type="password" id="password" name="password" onChange={(e) => this.handleChange(e)}></input>
+//                     </div><br></br>
+//                     <div className="signin-btn">
+//                         <input type="submit" value="Sign in"/>
+//                     </div>
+//                 </form>
+//                 <div>
+//                  <Link to="/signup">
+//                     <button>Don't have an account?</button>
+//                  </Link>
+//                  <Link to="/home">
+//                     <button>So we can still access home</button>
+//                  </Link>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default SignIn;
 /**
